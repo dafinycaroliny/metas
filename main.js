73bec6f524dffb95237a5bfd83d9b597b8c82308
dafinycaroliny <105360433+dafinycaroliny@users.noreply.github.com>
2024-03-11 10:07:22 -0300

    fok

--- main.js
+++ main.js
@@ -2,9 +2,11 @@ const botoes= document.querySelectorAll(".botao");
         //console.log(botoes);
         for(let i=0; i<botoes.length; i++){
             botoes[i].onclick=function(){
-                for(letj=0;<botoes.length;j++){
-                botoes[j].classList.remove("ativo");}
-                botoes[i].classList.add("ativo");}
+                for(let j=0;<botoes.length;j++){
+                botoes[j].classList.remove("ativo");
             }
+                botoes[i].classList.add("ativo");
+        }
+     }
         
         
