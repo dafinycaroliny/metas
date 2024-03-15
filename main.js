fb2719714b67e53f41d5b35c6c0c9610ed8acc48
dafinycaroliny <105360433+dafinycaroliny@users.noreply.github.com>
2024-03-15 11:10:17 -0300

    ok

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 const botoes= document.querySelectorAll(".botao");
-        //console.log(botoes);
-        for(let i=0; i<botoes.length; i++){
+        for(let i=0; i <botoes.length;i++){
             botoes[i].onclick=function(){
                 for(let j=0;j<botoes.length; j++){
                 botoes[j].classList.remove("ativo");
@@ -9,4 +8,5 @@ const botoes= document.querySelectorAll(".botao");
         }
      }
         
-        
+     
+    
