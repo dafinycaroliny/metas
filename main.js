2dd6f276812a143ef5978a711053fac7cb657141
dafinycaroliny <105360433+dafinycaroliny@users.noreply.github.com>
2024-03-15 11:22:36 -0300

    ok

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const texto = document.querySelectorAll(".aba-conteudo")
             botoes[i].onclick=function(){
                 for(let j=0;j<botoes.length;j++){
                 botoes[j].classList.remove("ativo");
+                texto[j].classList.remove("ativo");
             }
                 botoes[i].classList.add("ativo");
                 texto[i].classList.add("ativo");
