be526b57dab2a39acbfc9fe3ff135a466d2b5a7b
dafinycaroliny <105360433+dafinycaroliny@users.noreply.github.com>
2024-04-01 10:29:03 -0300

    xcvv

--- main.js
+++ main.js
@@ -17,8 +17,9 @@ for(let i=0; i <botoes.length;i++){
     
     //contadores[0].textContent = tempoObjetivo1 - tempoAtual;
      
-    //function calculaTempo(tempoObjetivo) 
-        let tempoAtual = new Date();
+    //function calculaTempo(tempoObjetivo) {
+        //}
+
         let tempoFinal = tempoObjetivo - tempoAtual;
         let segundos = math.floor(tempoFinal/1000);
         let minutos = Math.floor(segundos/60);
