0ea27cbd33454044117aa6b93db4b163ec672adb
dafinycaroliny <105360433+dafinycaroliny@users.noreply.github.com>
2024-04-01 10:24:01 -0300

    FGHJ,.

--- main.js
+++ main.js
@@ -14,7 +14,15 @@ for(let i=0; i <botoes.length;i++){
     const contadores = document.querySelectorAll(".contador");
     const tempoObjetivo1 = new Date("2024-12-31t00:00:00");
     let tempoAtual = new Date();
-    contadores[0].textContent = tempoObjetivo1 - tempoAtual;
-             
-     
     
+    //contadores[0].textContent = tempoObjetivo1 - tempoAtual;
+     
+    //function calculaTempo(tempoObjetivo) 
+        let tempoAtual = new Date();
+        let tempoFinal = tempoObjetivo - tempoAtual;
+        let segundos = math.floor(tempoFinal/1000);
+        let minutos = Math.floor(segundos/60);
+        let horas = Math.floor(minutos/60);
+        let dias = Math.floor(horas/24);
+
+    return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
