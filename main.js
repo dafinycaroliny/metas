0296a9b2e57dd7632239f3764855cd757a55be89
dafinycaroliny <105360433+dafinycaroliny@users.noreply.github.com>
2024-04-08 10:24:13 -0300

    ok

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ for(let i=0; i <botoes.length;i++){
     const tempoObjetivo4 = new Date("2024-02-01T00:00:00");
 
 
-    contadores[0].textContent = calculaTempo(tempoObjetivo1);
+    const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
      
     function calculaTempo(tempoObjetivo) {
         let tempoAtual = new Date();
@@ -31,7 +31,24 @@ for(let i=0; i <botoes.length;i++){
         segundos %= 60;
         minutos %= 60;
         horas %= 24;
-
+    
+        if (tempoFinal>0){
     return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
+}else{
+    return"Prazo Finalizado";
 }
-       
+    }
+
+    function atualizaCronometro(){
+        for (let i=0; i<contadores.length;i++){
+            contadores[i].textContent = calculaTempo(tempos[i]);   
+        }
+    }
+
+    function comecaCronometro(){
+        atualizaCronometro();
+        setInterval(atualizaCronometro,1000);
+    }
+    
+    comecaCronometro();
+    
