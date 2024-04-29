04585e45ebf73d1ce99acf0f7c06667d9e096b8a
dafinycaroliny <105360433+dafinycaroliny@users.noreply.github.com>
2024-04-29 10:40:11 -0300

    ok

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2024-12-21T00:00:00");
 const tempoObjetivo2 = new Date("2028-12-30T00:00:00");
 const tempoObjetivo3 = new Date("2025-12-31T00:00:00");
-const tempoObjetivo4 = new Date("2024-12-21T00:00:00");
+const tempoObjetivo4 = new Date("2024-07-30T00:00:00");
 
 
 const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
